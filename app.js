5a3ab38f6dd820d6c0650d65ff3b68ee2495c602
adam18092005 <adam18092005@gmail.com>
2022-01-07 14:28:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,89 @@
+let dictate= document.getElementById('dictate');
+let dictateTimeoutArr = [];
+
+// for pausing
+document.getElementById('pause').onclick = () =>{
+	for(let v=0; v<dictateTimeoutArr.length; v++){
+		clearTimeout(dictateTimeoutArr[v]);
+	}
+};
+
+
+
+// for adding dummy text
+addDummy = () => {
+	document.getElementById('text').value+="The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack.";
+};
+
+
+
+
+
+// for dictate
+
+var speech = new SpeechSynthesisUtterance();
+speech.rate = .6;
+speech.pitch = 1;
+speech.volume=1;
+
+for(let u=0;u<window.speechSynthesis.getVoices().length;u++){
+  if(window.speechSynthesis.getVoices()[u]['voiceURI'].search("David")!=-1){
+    speech.voice=window.speechSynthesis.getVoices()[u];
+  }
+}
+setTimeout(function(){
+  for(let u=0;u<window.speechSynthesis.getVoices().length;u++){
+    if(window.speechSynthesis.getVoices()[u]['voiceURI'].search("David")!=-1){
+      speech.voice=window.speechSynthesis.getVoices()[u];
+    }
+  }
+},300);
+
+function speakNow(MyText){
+  speech.text = MyText;
+  window.speechSynthesis.speak(speech);
+  console.log('[SPEAKING] '+speech.text);
+}
+
+
+dictate.onclick = () => {
+	setTimeout(function(){
+
+		let text= document.getElementById('text').value;
+		let arr = text.split(" ");
+
+		arr = arr.filter((curelem)=>{
+			return (curelem!='');
+		});
+
+		let afj = 0;
+		for(let i=0; i<arr.length; i+=3){
+			
+			let toBeSpoken = '';
+			if(arr[i+2] != undefined){
+				toBeSpoken = arr[i]+" "+arr[i+1]+" "+arr[i+2];
+			}else if(arr[i+1] != undefined){
+				toBeSpoken = arr[i]+" "+arr[i+1];
+			}else{
+				toBeSpoken = arr[i];
+			}
+			
+			let fop = setTimeout(()=>{
+				speakNow(toBeSpoken);
+				
+			}, afj*1000);
+
+			dictateTimeoutArr.push(fop);
+
+			let numOfCharsArr = toBeSpoken.split('').filter((curelem)=>{
+				return (curelem!=" ");
+			});
+			let gapBtwnSpokens = 0.52;
+			afj+=(numOfCharsArr.length)*gapBtwnSpokens;
+			
+		}
+
+	},2000);
+	
+
+};
